f122b10ea553b0bc412201a4e16219ccaa700328
Christoph Gysin <christoph.gysin@gmail.com>
2016-03-03 08:01:48 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bluetrust',
+      version='0',
+      description='Webinterface to trust bluetooth devices',
+      author='Christoph Gysin',
+      author_email='christoph.gysin@gmail.com',
+      url='https://github.com/christophgysin/bluetrust',
+      license='GPLv2',
+      scripts=['bluetrust.py'],
+      requires=['dbus (>=1.2.0)',
+                'gobject (>=2.28.3)',
+                'twisted (>=13.2.0)']
+     )
